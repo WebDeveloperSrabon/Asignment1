8490e3f0c5f3e5fc19f9646bfb90e38353d774b6
WebDeveloperSrabon <122147793+WebDeveloperSrabon@users.noreply.github.com>
2023-01-17 12:07:16 -0800

    fist commit

---
+++ AsignMent.js
@@ -0,0 +1,119 @@
+// ১. Variable কি ?
+
+// Ans: variable mane hosse value rakhar jaiga ba kono akta object ar name deya & data store
+
+// ২. Variable কিভাবে লিখতে হয় ?
+// All ans
+    var x = "hello world"
+    console.log(x)
+
+// ৩. string type variable কি ও কি ভাবে লিখতে হয় ?
+// All Ans
+var x = "srabon"
+console.log(x)
+
+
+// ৪. number type variable কি ও কি ভাবে লিখতে হয় ?
+// All ans
+var x = 123;
+console.log(x)
+
+// ৫. Boolan type variable  কি ও কি ভাবে লিখতে হয় ?
+// All Ans
+var boolType = true;
+console.log(boolType)
+var boolFalse = false;
+console.log(boolFalse)
+
+
+// ৬. toUpperCase() & toLowerCase() এর ব্যাবহার কি ভাবে করতে হয় ?
+
+// Ans:
+var srabon = "I Will Become a Web Developer in 2 months"
+console.log(srabon.toUpperCase())
+
+var srabonWeb = "I Will Become a Web Developer in 2 months"
+console.log(srabonWeb.toLowerCase())
+
+
+// ৭. JavaScript এর মোট কয়টি অপারেটর আছে ও কি কি ?
+
+// Ans: javascript ar 3 prokar: 1. Arithmatic Operator 2.Assignment Operator 3.Comparison Operator
+// All ans:
+// 1.
+var x = 10;
+var a = 15;
+console.log(x + a)
+// 2.
+var x = 15;
+var a = 10;
+console.log(x - a)
+// 3.
+var x = 15;
+var a = 10;
+var total = x * a;
+console.log(total)
+// 4.
+var x = 15;
+var a = 10;
+var sum = x / a;
+console.log(sum)
+// 5.
+var x = 15;
+var a = 10;
+var sum1 = x % a;
+console.log(sum1)
+// .6
+var x = 5;
+x++
+console.log(x)
+// .7
+var x = 5;
+x--
+console.log(x)
+
+
+
+
+// ৮. Math.abs() এর ব্যাবহার লিখুন ।
+// Ans
+var x = -10;
+var a = Math.abs(x)
+console.log(a)
+
+// ৯. Math.celi()  এর ব্যাবহার লিখুন ।
+// Ans
+var totalVote = 100;
+// var myVote = 65.55999;
+var myVote = 0.5555;
+var sum = Math.ceil(myVote)
+console.log(sum)
+
+
+
+// ১০. Math.Floor() এর ব্যাবহার লিখুন ।
+
+
+var x = 10;
+var vote = 6.9999;
+var sum1 = Math.floor(vote)
+console.log(sum1)
+
+
+// ১১. Math.round() এর ব্যাবহার লিখুন ।
+
+var x = 100;
+var voter = 65.444;
+var sum3 = Math.round(voter)
+console.log(sum3)
+
+
+// ১২. Math.random() এর ব্যাবহার লিখুন ।
+
+
+var last = 100;
+var random = Math.random() * 100
+console.log(random)
+
+
+// asignment.js - GitHub Link
